64f2e3341a24d4627a6ba0dbc676d7ac0c2ae889
Gilberto Junior <junior.siilva182@gmail.com>
2020-10-30 18:20:20 -0300

    This commit include the complete app

---
+++ README.md
@@ -0,0 +1,34 @@
+# Chat Online
+
+Aplicativo Web de chat online.
+
+![Principal Image](./client/src/images/readme-md-img.png)
+
+## Tecnologias
+
+Tecnologias usadas no projeto.
+
+* React
+* Node.js
+* Socket.io
+
+## Serviços Usados
+
+* Github
+* Flaticon
+  
+## Começando
+
+* Dependências
+
+  * Git
+  * NPM
+  * Yarn
+
+* Para rodar o projeto:
+
+> $ yarn start
+
+## Créditos
+
+Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
---
+++ server/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const cors = require('cors');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
+// app.use(cors()); // Uncomment for production deploy
+
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, bem-vindo a sala ${user.room} ;)`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name}, entrou! :D` });
+
+    socket.join(user.room);
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('message', {
+        user: 'admin',
+        text: `${user.name} saiu :(`,
+      });
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
